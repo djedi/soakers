11849f5c8871f4b0784107e18b84d4ffa178d9c2
Dustin Davis <dustin.davis@verizonmedia.com>
2020-10-02 11:33:20 -0600

    WIP

---
+++ src/_data/nav.js
@@ -0,0 +1,26 @@
+module.exports = {
+  pages: [
+    { name: "Home", slug: "home" },
+    {
+      name: "Spa Models",
+      slug: "spa-models",
+      pages: [
+        { name: "Artesian Elite", href: "https://artesianspas.com/artesian-elite/" },
+        { name: "Island & Island Elite", href: "https://artesianspas.com/island-spas/" },
+        { name: "South Seas Spas", href: "http://southseasspas.com/" },
+        { name: "Garden Spas", href: "http://thegardenspas.com/" },
+      ],
+    },
+    {
+      name: "Features",
+      slug: "features",
+      pages: [
+        { name: "Covers", href: "https://artesianspas.com/spa-accessories/covers/" },
+        {
+          name: "Health & Wellness",
+          href: "https://artesianspas.com/spa-features/health-wellness/",
+        },
+      ],
+    },
+  ],
+};
---
+++ src/pages/spa-models/artesian-elite.md
@@ -0,0 +1,12 @@
+---
+title: Artesian Elite
+---
+
+<iframe
+  width="1170"
+  height="658"
+  src="https://www.youtube.com/embed/UwbykPlSR-I"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+></iframe>
