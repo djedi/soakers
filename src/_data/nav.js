05a6b0b3b897e50192a54dcd01f5872d7b8a27cc
Dustin Davis <dustin.davis@verizonmedia.com>
2020-10-03 09:43:15 -0600

    WIP: add iframing

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -1,6 +1,6 @@
 module.exports = {
   pages: [
-    { name: "Home", slug: "home" },
+    // { name: "Home", slug: "" },
     {
       name: "Spa Models",
       slug: "spa-models",
@@ -20,7 +20,27 @@ module.exports = {
           name: "Health & Wellness",
           href: "https://artesianspas.com/spa-features/health-wellness/",
         },
+        {
+          name: "Quality Contruction",
+          href: "https://artesianspas.com/spa-features/quality-construction/",
+        },
+        {
+          name: "Hydrotherapy Jets",
+          href: "https://artesianspas.com/spa-features/hydrotherapy-jets/",
+        },
+        { name: "Water Features", href: "https://artesianspas.com/spa-features/water-features/" },
+        {
+          name: "Innovative Technoloyg",
+          href: "https://artesianspas.com/spa-features/innovative-technology/",
+        },
+        { name: "LED Lighting", href: "https://artesianspas.com/spa-features/led-lighting/" },
+        { name: "Stereo Systems", href: "https://artesianspas.com/spa-features/stereo-systems/" },
       ],
+    },
+    {
+      name: "Accessories",
+      slug: "accessories",
+      pages: [{ name: "Steps", href: "https://artesianspas.com/spa-accessories/steps/" }],
     },
   ],
 };
