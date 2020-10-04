3d9bcff400ea671f73873d219e39be1a998a09da
Dustin Davis <dustin.davis@verizonmedia.com>
2020-10-03 18:16:35 -0600

    WIP: menu

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -3,44 +3,71 @@ module.exports = {
     // { name: "Home", slug: "" },
     {
       name: "Spa Models",
-      slug: "spa-models",
+      href: "#",
       pages: [
-        { name: "Artesian Elite", href: "https://artesianspas.com/artesian-elite/" },
-        { name: "Island & Island Elite", href: "https://artesianspas.com/island-spas/" },
-        { name: "South Seas Spas", href: "http://southseasspas.com/" },
-        { name: "Garden Spas", href: "http://thegardenspas.com/" },
+        { name: "Artesian Elite", href: "https://artesianspas.com/artesian-elite/", framed: true },
+        {
+          name: "Island & Island Elite",
+          href: "https://artesianspas.com/island-spas/",
+          framed: true,
+        },
+        { name: "South Seas Spas", href: "http://southseasspas.com/", framed: true },
+        { name: "Garden Spas", href: "http://thegardenspas.com/", framed: true },
+        { name: "Swim Spas", href: "https://tidalfit.com/", framed: true },
       ],
     },
     {
       name: "Features",
-      slug: "features",
+      href: "#",
       pages: [
-        { name: "Covers", href: "https://artesianspas.com/spa-accessories/covers/" },
+        { name: "Covers", href: "https://artesianspas.com/spa-accessories/covers/", framed: true },
         {
           name: "Health & Wellness",
           href: "https://artesianspas.com/spa-features/health-wellness/",
+          framed: true,
         },
         {
           name: "Quality Contruction",
           href: "https://artesianspas.com/spa-features/quality-construction/",
+          framed: true,
         },
         {
           name: "Hydrotherapy Jets",
           href: "https://artesianspas.com/spa-features/hydrotherapy-jets/",
+          framed: true,
         },
-        { name: "Water Features", href: "https://artesianspas.com/spa-features/water-features/" },
+        {
+          name: "Water Features",
+          href: "https://artesianspas.com/spa-features/water-features/",
+          framed: true,
+        },
         {
           name: "Innovative Technoloyg",
           href: "https://artesianspas.com/spa-features/innovative-technology/",
+          framed: true,
         },
-        { name: "LED Lighting", href: "https://artesianspas.com/spa-features/led-lighting/" },
-        { name: "Stereo Systems", href: "https://artesianspas.com/spa-features/stereo-systems/" },
+        {
+          name: "LED Lighting",
+          href: "https://artesianspas.com/spa-features/led-lighting/",
+          framed: true,
+        },
+        {
+          name: "Stereo Systems",
+          href: "https://artesianspas.com/spa-features/stereo-systems/",
+          framed: true,
+        },
       ],
     },
     {
       name: "Accessories",
-      slug: "accessories",
-      pages: [{ name: "Steps", href: "https://artesianspas.com/spa-accessories/steps/" }],
+      href: "#",
+      pages: [
+        { name: "Steps", href: "https://artesianspas.com/spa-accessories/steps/", framed: true },
+      ],
     },
+    { name: "Design Your Spa", href: "https://artesianspas.com/design-your-spa/", framed: true },
+    { name: "Saunas", href: "https://www.helosaunas.com/", framed: true },
+    { name: "Desert Steel", href: "https://desertsteel.net/", newTab: true },
+    { name: "Saber Grills", href: "https://www.sabergrills.com/", newTab: true },
   ],
 };
